be8390dc4b3bed1b0f3f7fdf7dafab6393eb828a
eduardo kochler <92959821+eduardokochler@users.noreply.github.com>
2022-08-16 12:47:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//Insere o numero
+function insert(number){
+    var numero = document.getElementById('Resultado').innerHTML; 
+    document.getElementById('Resultado').innerHTML = numero + number
+ }
+ //Limpa a calculadora
+ function clean(){
+     document.getElementById('Resultado').innerHTML = "";
+ }
+ //Apaga um numero
+ function back(){
+     var Resultado = document.getElementById('Resultado').innerHTML;
+     document.getElementById('Resultado').innerHTML = Resultado.substring(0,Resultado.length - 1)
+ }
+ //Calcula a operacao
+ function calcular(){
+     var Resultado = document.getElementById('Resultado').innerHTML;
+     if(Resultado){
+         document.getElementById('Resultado').innerHTML = eval(Resultado)
+     }
+     else{
+         document.getElementById('Resultado').innerHTML = ''
+     }
+ }
